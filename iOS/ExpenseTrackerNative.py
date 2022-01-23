e1738d507c5b5a54a4f47a6e50a0b40258b5fb26
Santhosh Rakunde <rakundes@gmail.com>
2022-01-23 16:49:48 +0530

    Third commit.

--- iOS/ExpenseTrackerNative.py
+++ iOS/ExpenseTrackerNative.py
@@ -8,29 +8,30 @@ from perfecto import model
 from selenium.webdriver.support.wait import WebDriverWait
 
 # 1. Replace "<Cloud Name>" with your cloud name (e.g, trial is the cloudName of trial.app.perfectomobile.com)
-cloudName = "<Cloud Name>"
+# cloudName = "<Cloud Name>"
 
 capabilities = {
     #  2. Replace <<security token>> with your perfecto security token.
     'securityToken': "<security token>",
 
     # 3. Set device capabilities.
-    'platformName': 'Android',
-    'model': 'Galaxy S.*|S.*',
+    'platformName': 'iOS',
+    'model': 'iPhone.*',
 
     # 4. Set Perfecto Media repository path of App under test.
-    'app': 'PRIVATE:ExpenseTrackerNative.apk',
+    'app': 'PRIVATE:ExpenseTrackerNative.ipa',
 
     # 5. Set the unique identifier of your app
-    'appPackage': 'io.perfecto.expense.tracker',
+    'bundleId': 'io.perfecto.expense.tracker',
 
     #Script Name
-    'scriptName': 'PythonAndroidNative',
+    'scriptName': 'PythoniOSNative',
 
     # Set other capabilities.
     'enableAppiumBehavior': True,  # Enable new architecture of Appium
     'autoLaunch': True,  # Whether to have Appium install and launch the app automatically.
     # 'fullReset': false, # Reset app state by uninstalling app
+    'iOSResign': True, # Resigns using developer certificate
     'takesScreenshot': False,
     'screenshotOnError': True,
     'openDeviceTimeout': 5
@@ -53,59 +54,60 @@ cf2 = model.CustomField('CustomTag2', 'Customvalue2')
 perfecto_execution_context = PerfectoExecutionContext(webdriver=driver,
                                                               tags=['Tag1', 'Tag2'],
                                                               job=model.Job('ExpenseJob', '1', 'MainBranch'),
-                                                              project=model.Project('ExpenseAndroidNative', '1.0'),
+                                                              project=model.Project('ExpenseiOSNative', '1.0'),
                                                               customFields=[cf1,cf2])
 
 reporting_client = PerfectoReportiumClient(perfecto_execution_context)
 print("Reporting client created")
 
 # Test start
-reporting_client.test_start('ExpenseNativeAndroidPython', TestContext(customFields=[cf1, cf2], tags=['Native', 'Android']))
+reporting_client.test_start('ExpenseNativeiOSPython', TestContext(customFields=[cf1, cf2], tags=['Native', 'iOS']))
 
 try:
     reporting_client.step_start("Enter Email")
-    email = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/login_email")))
+    email = wait.until(EC.presence_of_element_located((MobileBy.NAME, "login_email")))
     email.send_keys('test@perfecto.com')
     reporting_client.step_end()
 
     reporting_client.step_start("Enter Password")
-    password = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/login_password")))
+    password = wait.until(EC.presence_of_element_located((MobileBy.NAME, "login_password")))
     password.send_keys('test123')
     reporting_client.step_end()
 
     reporting_client.step_start("Click on Login")
-    login = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/login_login_btn")))
+    login = wait.until(EC.presence_of_element_located((MobileBy.NAME, "login_login_btn")))
     login.click()
-    actualText1 = driver.find_element_by_xpath("//*[@text=\"Expenses\"]").text
+    actualText1 = driver.find_element_by_xpath("//*[@label='Expenses']").text
     reporting_client.reportium_assert('Login successful', actualText1 == "Expenses")
     reporting_client.step_end()
 
     reporting_client.step_start("Click on Add(+)")
-    add = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/list_add_btn")))
+    add = wait.until(EC.presence_of_element_located((MobileBy.NAME, "list_add_btn")))
     add.click()
     reporting_client.step_end()
 
     reporting_client.step_start("Click on Head/Select")
-    head = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/input_layout_head")))
+    head = wait.until(EC.presence_of_element_located((MobileBy.NAME, "edit_head")))
     head.click()
     reporting_client.step_end()
 
     reporting_client.step_start("Click on Flight")
-    flight = wait.until(EC.presence_of_element_located((MobileBy.XPATH, '//*[@text="Flight"]')))
-    flight.click()
+    flight = wait.until(EC.presence_of_element_located((MobileBy.XPATH, "//*[@value='- Select -']")))
+    flight.send_keys("Flight")
     reporting_client.step_end()
 
     reporting_client.step_start("Enter amount")
-    amount = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/add_amount")))
+    amount = wait.until(EC.presence_of_element_located((MobileBy.NAME, "edit_amount")))
     amount.send_keys('100')
     reporting_client.step_end()
 
     reporting_client.step_start("Click on Save and verify alert")
-    save = wait.until(EC.presence_of_element_located((MobileBy.ID, "io.perfecto.expense.tracker:id/layout_buttons")))
+    save = wait.until(EC.presence_of_element_located((MobileBy.NAME, "add_save_btn")))
     save.click()
-    actualText2 = driver.find_element_by_xpath("//*[@resource-id='io.perfecto.expense.tracker:id/snackbar_text']").text
-    reporting_client.reportium_assert('Verify Alert text', actualText2 == "Select Currency")
-
+    time.sleep(3)
+    #Execute script method to find text
+    params = { 'content': 'Please enter valid category'}
+    driver.execute_script('mobile:text:find', params);
     reporting_client.step_end()
 
     reporting_client.test_stop(TestResultFactory.create_success())
@@ -125,4 +127,4 @@ finally:
     except Exception as e:
         print(e)
 
-print('Android Python Test run ended')
+print('iOS Python Test run ended')
